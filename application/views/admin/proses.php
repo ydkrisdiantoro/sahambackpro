31d66b8dfed4d8b998ed17549ebb40eb3da7de88
Yayan Dwi Krisdiantoro <y.d.krisdiantoro@gmail.com>
2020-12-26 22:06:49 +0700

    fix tampilan normalisasi

--- application/views/admin/proses.php
+++ application/views/admin/proses.php
@@ -36,77 +36,83 @@
                         </div>
                         <div class="card-body">
 
-                            <h3 class="card-title">1. Normalisasi Data</h3>
-                            <table id="" class="table table-bordered table-striped">
-                                <thead>
-                                    <th>Tanggal</th>
-                                    <th>Data Terakhir</th>
-                                    <th>Data Pembukaan</th>
-                                    <th>Data Tertinggi</th>
-                                    <th>Data Terendah</th>
-                                    <th>Volume</th>
-                                    <th>Perubahan</th>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $data = array();
-                                    foreach ($listdata as $a) {
-                                        $data["tanggal"][] = $a["tanggal"];
-                                        $data["terakhir"][] = $a["terakhir"];
-                                        $data["pembukaan"][] = $a["pembukaan"];
-                                        $data["tertinggi"][] = $a["tertinggi"];
-                                        $data["terendah"][] = $a["terendah"];
-                                        $volume = explode("B", $a["volume"]);
-                                        // print_r($volume);
-                                        $data["volume"][] = $volume[0];
-                                        $data["perubahan"][] = $a["perubahan"];
-                                    }
-                                    $terakhir_max = max($data["terakhir"]);
-                                    $pembukaan_max = max($data["pembukaan"]);
-                                    $tertinggi_max = max($data["tertinggi"]);
-                                    $terendah_max = max($data["terendah"]);
-                                    $volume_max = max($data["volume"]);
-                                    $perubahan_max = max($data["perubahan"]);
+                            <div class="row">
+                                <div class="col-12 mb-3">
+                                    <h3 class="card-title alert alert-info">1. Normalisasi Data</h3>
+                                </div>
+                                <div class="col-12">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                            <th>Tanggal</th>
+                                            <th>Data Terakhir</th>
+                                            <th>Data Pembukaan</th>
+                                            <th>Data Tertinggi</th>
+                                            <th>Data Terendah</th>
+                                            <th>Volume</th>
+                                            <th>Perubahan</th>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $data = array();
+                                            foreach ($listdata as $a) {
+                                                $data["tanggal"][] = $a["tanggal"];
+                                                $data["terakhir"][] = $a["terakhir"];
+                                                $data["pembukaan"][] = $a["pembukaan"];
+                                                $data["tertinggi"][] = $a["tertinggi"];
+                                                $data["terendah"][] = $a["terendah"];
+                                                $volume = explode("B", $a["volume"]);
+                                                // print_r($volume);
+                                                $data["volume"][] = $volume[0];
+                                                $data["perubahan"][] = $a["perubahan"];
+                                            }
+                                            $terakhir_max = max($data["terakhir"]);
+                                            $pembukaan_max = max($data["pembukaan"]);
+                                            $tertinggi_max = max($data["tertinggi"]);
+                                            $terendah_max = max($data["terendah"]);
+                                            $volume_max = max($data["volume"]);
+                                            $perubahan_max = max($data["perubahan"]);
 
-                                    $terakhir_min = min($data["terakhir"]);
-                                    $pembukaan_min = min($data["pembukaan"]);
-                                    $tertinggi_min = min($data["tertinggi"]);
-                                    $terendah_min = min($data["terendah"]);
-                                    $volume_min = min($data["volume"]);
-                                    $perubahan_min = min($data["perubahan"]);
+                                            $terakhir_min = min($data["terakhir"]);
+                                            $pembukaan_min = min($data["pembukaan"]);
+                                            $tertinggi_min = min($data["tertinggi"]);
+                                            $terendah_min = min($data["terendah"]);
+                                            $volume_min = min($data["volume"]);
+                                            $perubahan_min = min($data["perubahan"]);
 
-                                    for ($i = 0; $i < count($data["tanggal"]); $i++) {
-                                        $hasil_terakhir = round(($data["terakhir"][$i] - $terakhir_min) / ($terakhir_max - $terakhir_min), 6);
-                                        $hasil_pembukaan = round(($data["pembukaan"][$i] - $pembukaan_min) / ($pembukaan_max - $pembukaan_min), 6);
-                                        $hasil_tertinggi = round(($data["tertinggi"][$i] - $tertinggi_min) / ($tertinggi_max - $tertinggi_min), 6);
-                                        $hasil_terendah = round(($data["terendah"][$i] - $terendah_min) / ($terendah_max - $terendah_min), 6);
-                                        $hasil_perubahan = round(($data["perubahan"][$i] - $perubahan_min) / ($perubahan_max - $perubahan_min), 6);
-                                        $hasil_volume = round(($data["volume"][$i] - $volume_min) / ($volume_max - $volume_min), 6);
-                                    ?>
-                                        <tr>
-                                            <td><?php echo $data['tanggal'][$i]; ?></td>
-                                            <td><?php echo $hasil_terakhir; ?></td>
-                                            <td><?php echo $hasil_pembukaan; ?></td>
-                                            <td><?php echo $hasil_tertinggi; ?></td>
-                                            <td><?php echo $hasil_terendah; ?></td>
-                                            <td><?php echo $hasil_volume; ?></td>
-                                            <td><?php echo $hasil_perubahan; ?></td>
+                                            for ($i = 0; $i < count($data["tanggal"]); $i++) {
+                                                $hasil_terakhir = round(($data["terakhir"][$i] - $terakhir_min) / ($terakhir_max - $terakhir_min), 6);
+                                                $hasil_pembukaan = round(($data["pembukaan"][$i] - $pembukaan_min) / ($pembukaan_max - $pembukaan_min), 6);
+                                                $hasil_tertinggi = round(($data["tertinggi"][$i] - $tertinggi_min) / ($tertinggi_max - $tertinggi_min), 6);
+                                                $hasil_terendah = round(($data["terendah"][$i] - $terendah_min) / ($terendah_max - $terendah_min), 6);
+                                                $hasil_perubahan = round(($data["perubahan"][$i] - $perubahan_min) / ($perubahan_max - $perubahan_min), 6);
+                                                $hasil_volume = round(($data["volume"][$i] - $volume_min) / ($volume_max - $volume_min), 6);
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $data['tanggal'][$i]; ?></td>
+                                                    <td><?php echo $hasil_terakhir; ?></td>
+                                                    <td><?php echo $hasil_pembukaan; ?></td>
+                                                    <td><?php echo $hasil_tertinggi; ?></td>
+                                                    <td><?php echo $hasil_terendah; ?></td>
+                                                    <td><?php echo $hasil_volume; ?></td>
+                                                    <td><?php echo $hasil_perubahan; ?></td>
 
-                                        </tr>
-                                    <?php } ?>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>Tanggal</th>
-                                        <th>Data Terakhir</th>
-                                        <th>Data Pembukaan</th>
-                                        <th>Data Tertinggi</th>
-                                        <th>Data Terendah</th>
-                                        <th>Volume</th>
-                                        <th>Perubahan</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Tanggal</th>
+                                                <th>Data Terakhir</th>
+                                                <th>Data Pembukaan</th>
+                                                <th>Data Tertinggi</th>
+                                                <th>Data Terendah</th>
+                                                <th>Volume</th>
+                                                <th>Perubahan</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
 
                         </div>
                         <!-- /.card-body -->
