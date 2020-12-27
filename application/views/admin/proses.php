f01b656ad5836a6d79709e55b66188f864309742
Yayan Dwi Krisdiantoro <y.d.krisdiantoro@gmail.com>
2020-12-27 11:40:03 +0700

    proses 2. inisialisasi bobot awal

--- application/views/admin/proses.php
+++ application/views/admin/proses.php
@@ -39,7 +39,7 @@
                             <div class="row">
 
                                 <div class="col-12 mb-3">
-                                    <h3 class="card-title alert alert-info">1. Normalisasi Data</h3>
+                                    <h3 class="card-title alert alert-primary">1. Normalisasi Data</h3>
                                 </div>
                                 <div class="col-12">
                                     <table id="normalisasi" class="display table table-bordered table-striped">
@@ -122,67 +122,40 @@
                                 </div>
 
                                 <div class="col-12 my-3">
-                                    <h3 class="card-title alert alert-info">2. Inisialisasi Bobot Awal</h3>
+                                    <h3 class="card-title alert alert-secondary">2. Inisialisasi Bobot Awal</h3>
                                 </div>
                                 <div class="col-12">
                                     <table id="inisialisasi_bobot_awal" class="display table table-bordered table-striped">
                                         <thead>
                                             <th>Tanggal</th>
                                             <th>Xi</th>
-                                            <th>Data Terakhir (Z1)</th>
-                                            <th>Data Pembukaan (Z2)</th>
-                                            <th>Data Tertinggi (Z3)</th>
-                                            <th>Data Terendah (Z4)</th>
-                                            <th>Volume (Z5)</th>
-                                            <th>Perubahan (Z6)</th>
+                                            <th>(Z1)</th>
+                                            <th>(Z2)</th>
+                                            <th>(Z3)</th>
+                                            <th>(Z4)</th>
+                                            <th>(Z5)</th>
+                                            <th>(Z6)</th>
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $data = array();
-                                            foreach ($listdata as $a) {
-                                                $data["tanggal"][] = $a["tanggal"];
-                                                $data["terakhir"][] = $a["terakhir"];
-                                                $data["pembukaan"][] = $a["pembukaan"];
-                                                $data["tertinggi"][] = $a["tertinggi"];
-                                                $data["terendah"][] = $a["terendah"];
-                                                $volume = explode("B", $a["volume"]);
-                                                // print_r($volume);
-                                                $data["volume"][] = $volume[0];
-                                                $data["perubahan"][] = $a["perubahan"];
+                                            function random()
+                                            {
+                                                $random = rand(0, 10);
+                                                $hasil = $random / 10;
+                                                return $hasil;
                                             }
-                                            $terakhir_max = max($data["terakhir"]);
-                                            $pembukaan_max = max($data["pembukaan"]);
-                                            $tertinggi_max = max($data["tertinggi"]);
-                                            $terendah_max = max($data["terendah"]);
-                                            $volume_max = max($data["volume"]);
-                                            $perubahan_max = max($data["perubahan"]);
-
-                                            $terakhir_min = min($data["terakhir"]);
-                                            $pembukaan_min = min($data["pembukaan"]);
-                                            $tertinggi_min = min($data["tertinggi"]);
-                                            $terendah_min = min($data["terendah"]);
-                                            $volume_min = min($data["volume"]);
-                                            $perubahan_min = min($data["perubahan"]);
-
-                                            for ($i = 0; $i < count($data["tanggal"]); $i++) {
-                                                $hasil_terakhir = round(($data["terakhir"][$i] - $terakhir_min) / ($terakhir_max - $terakhir_min), 6);
-                                                $hasil_pembukaan = round(($data["pembukaan"][$i] - $pembukaan_min) / ($pembukaan_max - $pembukaan_min), 6);
-                                                $hasil_tertinggi = round(($data["tertinggi"][$i] - $tertinggi_min) / ($tertinggi_max - $tertinggi_min), 6);
-                                                $hasil_terendah = round(($data["terendah"][$i] - $terendah_min) / ($terendah_max - $terendah_min), 6);
-                                                $hasil_perubahan = round(($data["perubahan"][$i] - $perubahan_min) / ($perubahan_max - $perubahan_min), 6);
-                                                $hasil_volume = round(($data["volume"][$i] - $volume_min) / ($volume_max - $volume_min), 6);
+                                            for ($i = 0; $i < count($data_normal["tanggal"]); $i++) {
                                                 $no_xi = $i + 1;
                                             ?>
                                                 <tr>
-                                                    <td><?php echo $data['tanggal'][$i]; ?></td>
+                                                    <td><?php echo $data_normal['tanggal'][$i]; ?></td>
                                                     <td><?php echo $no_xi; ?></td>
-                                                    <td><?php echo $hasil_terakhir; ?></td>
-                                                    <td><?php echo $hasil_pembukaan; ?></td>
-                                                    <td><?php echo $hasil_tertinggi; ?></td>
-                                                    <td><?php echo $hasil_terendah; ?></td>
-                                                    <td><?php echo $hasil_volume; ?></td>
-                                                    <td><?php echo $hasil_perubahan; ?></td>
-
+                                                    <td><?php echo random(); ?></td>
+                                                    <td><?php echo random(); ?></td>
+                                                    <td><?php echo random(); ?></td>
+                                                    <td><?php echo random(); ?></td>
+                                                    <td><?php echo random(); ?></td>
+                                                    <td><?php echo random(); ?></td>
                                                 </tr>
                                             <?php } ?>
                                         </tbody>
